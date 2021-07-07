b87c7d8115cbe724b9ff21759c872599c23a24e9
DanLowo <danlowo@horbify.com>
2021-07-07 23:04:35 +0100

    proper login & verification

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="register">
+  <div>
     <div class="auth-header" align="center">
       <span class="close-icon">
         <v-icon @click="$router.back()">mdi-close</v-icon>
@@ -7,152 +7,158 @@
       <span class="fa fa-user-o mb-3" style="font-size: 40px"></span>
       <p class="font-weight-bold">REGISTER</p>
     </div>
+    <div class="register" v-show="!registered">
+      <div class="mt-14 mx-6">
+        <form @submit.prevent="submit">
+          <label for="firstName">
+            <b>First name</b>
+          </label>
+          <input
+            type="text"
+            required
+            v-model="firstName"
+            class="zameria-light-form"
+            placeholder="First name"
+            id="firstName"
+          />
 
-    <div class="mt-14 mx-6">
-      <form @submit.prevent="submit">
-        <label for="firstName">
-          <b>First name</b>
-        </label>
-        <input
-          type="text"
-          required
-          v-model="firstName"
-          class="zameria-light-form"
-          placeholder="First name"
-          id="firstName"
-        />
+          <label for="lastName">
+            <b>Last name</b>
+          </label>
+          <input
+            type="text"
+            required
+            v-model="lastName"
+            class="zameria-light-form"
+            placeholder="Last name"
+            id="lastName"
+          />
 
-        <label for="lastName">
-          <b>Last name</b>
-        </label>
-        <input
-          type="text"
-          required
-          v-model="lastName"
-          class="zameria-light-form"
-          placeholder="Last name"
-          id="lastName"
-        />
+          <label for="number">
+            <b>Phone Number</b>
+          </label>
+          <input
+            type="number"
+            required
+            v-model="phone_no"
+            class="zameria-light-form"
+            placeholder="Phone Number"
+            id="number"
+          />
 
-        <label for="number">
-          <b>Phone Number</b>
-        </label>
-        <input
-          type="number"
-          required
-          v-model="phone_no"
-          class="zameria-light-form"
-          placeholder="Phone Number"
-          id="number"
-        />
+          <label for="email">
+            <b>Email address</b>
+          </label>
+          <input
+            type="email"
+            required
+            v-model="email"
+            class="zameria-light-form"
+            placeholder="Email address"
+            id="email"
+          />
 
-        <label for="email">
-          <b>Email address</b>
-        </label>
-        <input
-          type="email"
-          required
-          v-model="email"
-          class="zameria-light-form"
-          placeholder="Email address"
-          id="email"
-        />
+          <label for="password">
+            <b>Password</b>
+          </label>
+          <span @click="changePasswordFieldType" :class="passwordFieldIcon"></span>
+          <input
+            :type="passwordFieldType"
+            required
+            v-model="password"
+            class="zameria-light-form mt-0 mb-1"
+            placeholder="Password"
+            id="password"
+          />
+          <span
+            v-show="passwordLengthValid"
+            style="font-size: 13px"
+            class="fa fa-exclamation-circle red--text"
+          >Your password needs to be at least 6 characters.</span>
 
-        <label for="password">
-          <b>Password</b>
-        </label>
-        <span @click="changePasswordFieldType" :class="passwordFieldIcon"></span>
-        <input
-          :type="passwordFieldType"
-          required
-          v-model="password"
-          class="zameria-light-form mt-0 mb-1"
-          placeholder="Password"
-          id="password"
-        />
-        <span
-          v-show="passwordLengthValid"
-          style="font-size: 13px"
-          class="fa fa-exclamation-circle red--text"
-        >Your password needs to be at least 6 characters.</span>
+          <div class="mt-4"></div>
+          <label for="confirmPassword">
+            <b>Confirm Password</b>
+          </label>
+          <span @click="changePasswordFieldType1" :class="passwordFieldIcon1"></span>
+          <input
+            :type="passwordFieldType1"
+            required
+            v-model="confirmPassword"
+            class="zameria-light-form mb-1"
+            placeholder="Confirm Password"
+            id="confirmPassword"
+          />
+          <span
+            v-show="match"
+            style="font-size: 13px"
+            class="fa fa-exclamation-circle red--text"
+          >Passwords doesn't match...</span>
 
-        <div class="mt-4"></div>
-        <label for="confirmPassword">
-          <b>Confirm Password</b>
-        </label>
-        <span @click="changePasswordFieldType1" :class="passwordFieldIcon1"></span>
-        <input
-          :type="passwordFieldType1"
-          required
-          v-model="confirmPassword"
-          class="zameria-light-form mb-1"
-          placeholder="Confirm Password"
-          id="confirmPassword"
-        />
-        <span
-          v-show="match"
-          style="font-size: 13px"
-          class="fa fa-exclamation-circle red--text"
-        >Passwords doesn't match...</span>
+          <p style="font-size: 14px" class="font-weight-bold mt-7 mb-2">
+            What are you mostly intrested in?
+            <span style="font-size: 11px">(optional)</span>
+          </p>
+          <p
+            style="font-size: 12px"
+          >We'll use the information to make more personalized recommendations.</p>
 
-        <p style="font-size: 14px" class="font-weight-bold mt-7 mb-2">
-          What are you mostly intrested in?
-          <span style="font-size: 11px">(optional)</span>
-        </p>
-        <p
-          style="font-size: 12px"
-        >We'll use the information to make more personalized recommendations.</p>
+          <input type="checkbox" v-model="preference.women" name="women" id="women" />
+          <label for="women">Women's fashion</label>
+          <br />
+          <input type="checkbox" v-model="preference.men" name="men" id="men" />
+          <label for="men">Men's fashion</label>
+          <br />
+          <input type="checkbox" v-model="preference.kids" name="kids" id="kids" />
+          <label for="kids">Kid's fashion</label>
 
-        <input type="checkbox" v-model="preference.women" name="women" id="women" />
-        <label for="women">Women's fashion</label>
-        <br />
-        <input type="checkbox" v-model="preference.men" name="men" id="men" />
-        <label for="men">Men's fashion</label>
-        <br />
-        <input type="checkbox" v-model="preference.kids" name="kids" id="kids" />
-        <label for="kids">Kid's fashion</label>
+          <v-divider class="mt-5 mb-5"></v-divider>
 
-        <v-divider class="mt-5 mb-5"></v-divider>
+          <input v-model="emailList" type="checkbox" name="emailList" id="emailList" />
+          <label
+            for="emailList"
+          >Yes, I wish to recieve emails about special offers, new products and exclusive promotions, I can cancel my subscription at any time. (optional)</label>
 
-        <input v-model="emailList" type="checkbox" name="emailList" id="emailList" />
-        <label
-          for="emailList"
-        >Yes, I wish to recieve emails about special offers, new products and exclusive promotions, I can cancel my subscription at any time. (optional)</label>
+          <br />
+          <br />
+          <input required type="checkbox" name="agreements" id="agreements" />
+          <label for="agreements">
+            Yes, I agree to the
+            <nuxt-link to="/agreements/terms-of-use">Terms and Conditions</nuxt-link>and Zameria's
+            <nuxt-link to="/agreements/privacy-policy">Privacy Policy.</nuxt-link>
+          </label>
 
-        <br />
-        <br />
-        <input required type="checkbox" name="agreements" id="agreements" />
-        <label for="agreements">
-          Yes, I agree to the
-          <nuxt-link to="/agreements/terms-of-use">Terms and Conditions</nuxt-link>and Zameria's
-          <nuxt-link to="/agreements/privacy-policy">Privacy Policy.</nuxt-link>
-        </label>
+          <v-btn
+            class="mt-7 text-capitalize"
+            height="43"
+            type="submit"
+            dark
+            depressed
+            color="ZameriaRed"
+            block
+          >Register</v-btn>
+        </form>
 
+        <authStrategy align="center" title="Register"></authStrategy>
+      </div>
+      <div align="center" style="height: 35vh" class="mt-10 mb-7 px-7 pt-9">
+        <p class="font-weight-bold">ALREADY A CUSTOMER?</p>
         <v-btn
-          class="mt-7 text-capitalize"
-          height="43"
-          type="submit"
+          tile
+          color="black"
           dark
           depressed
-          color="ZameriaRed"
           block
-        >Register</v-btn>
-      </form>
-
-      <authStrategy align="center" title="Register"></authStrategy>
+          to="/auth/login"
+          height="43"
+          class="mt-2 text-capitalize"
+        >Login here</v-btn>
+      </div>
     </div>
-    <div align="center" style="height: 35vh" class="mt-10 mb-7 px-7 pt-9">
-      <p class="font-weight-bold">ALREADY A CUSTOMER?</p>
-      <v-btn
-        tile
-        color="black"
-        dark
-        depressed
-        block
-        to="/auth/login"
-        height="43"
-        class="mt-2 text-capitalize"
-      >Login here</v-btn>
+
+    <div v-show="registered" id="verify-account" align="center">
+      <h1>ACCOUNT CREATED</h1>
+      <p>Please check you email to verify your account</p>
     </div>
   </div>
 </template>
@@ -183,7 +189,8 @@ export default {
       passwordFieldType: "password",
       passwordFieldType1: "password",
       passwordFieldIcon: "fa fa-eye-slash input-icon",
-      passwordFieldIcon1: "fa fa-eye-slash input-icon"
+      passwordFieldIcon1: "fa fa-eye-slash input-icon",
+      registered: true
     };
   },
 
@@ -196,11 +203,11 @@ export default {
       }
     },
 
-    confirmPassword: function(){
-      if(this.password !== this.confirmPassword){
-        this.match = true
+    confirmPassword: function() {
+      if (this.password !== this.confirmPassword) {
+        this.match = true;
       } else {
-        this.match = false
+        this.match = false;
       }
     }
   },
@@ -239,21 +246,14 @@ export default {
       };
 
       try {
-        let create = await this.$axios.post(
+        let { data } = await this.$axios.post(
           "/accounts/signup/create_account/",
           details
         );
-
-        let data = {
-          email_or_username : this.email,
-          password : this.password
-        }
-
-        let res = await this.$auth.loginWith("local", {
-          data: data,
-        });
+        console.log(data);
+        this.registered = true;
       } catch (err) {
-        console.log("e");
+        console.log(err);
       }
     }
   }
