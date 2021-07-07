f78a9fbec44bf973ebf1957fe2847bdec62ca5e8
DanLowo <danlowo@horbify.com>
2021-07-07 23:04:42 +0100

    token page

---
+++ pages/auth/verify_account/_token.vue
@@ -0,0 +1,25 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  async fetch() {
+      let token = this.$route.params.token;
+      console.log(token)
+      try {
+          let data = this.$axios.get(`/accounts/activate_account/${token}/`)
+
+          this.$router.push("/auth/login");
+      } catch (err) {
+          console.log(err)
+      }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
