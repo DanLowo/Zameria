5e1420a61174b35f272cdcedca2c8d90798bd7b1
DanLowo <danlowo@horbify.com>
2021-07-12 15:12:18 +0100

    send email address to get authCode

--- pages/auth/reset-password.vue
+++ pages/auth/reset-password.vue
@@ -11,7 +11,9 @@
         style="font-size: 15px"
       >That's ok - enter the email address linked to you Zameria account. If you're in our system, we'll send you a link to reset your password.</p>
 
-      <form class="mt-5" @submit.prevent="submit">
+      <Alert v-show="alert.show" :message="alert.message" :type="alert.type"/>
+
+      <form class="mt-5" @submit.prevent="submitEmail" v-if="!isCode">
         <label for="email">
           <b>Email address</b>
         </label>
@@ -27,6 +29,22 @@
         <v-btn class="text-capitalize" height="43" type="submit" dark depressed color="ZameriaRed" block>Reset Password</v-btn>
       </form>
 
+      <form class="mt-5" @submit.prevent="submitCode" v-if="isCode">
+        <label for="email">
+          <b>Auth Code</b>
+        </label>
+        <input
+          type="number"
+          required
+          v-model="authCode"
+          class="zameria-light-form"
+          placeholder="Email address"
+          id="authCode"
+        />
+
+        <v-btn class="text-capitalize" height="43" type="submit" dark depressed color="ZameriaRed" block>Verify Code</v-btn>
+      </form>
+
       <p align="center" class="mt-8 font-weight-bold" style="font-size: 14px">
         <nuxt-link to="/auth/login" style="color: black"> Back to Login</nuxt-link>
       </p>
@@ -35,14 +53,46 @@
 </template>
 
 <script>
+import Alert from "@/components/alert";
 export default {
+  components: {
+    Alert
+  },
   data() {
     return {
-      email: ""
+      email: "",
+      isCode: false,
+      alert: {
+        show: false,
+        type: '',
+        message: ''
+      }
     };
   },
   methods: {
-    submit() {}
+    async submitEmail() {
+      this.alert = {
+        show: false,
+        type: "",
+        message: ""
+      }
+      try {
+        let { data } = await this.$axios.post('/accounts/forgotpassword/1/', {
+          email: this.email
+        })
+        this.alert = {
+          show: true,
+          type: 'success',
+          message: "Check your email for verification code"
+        }
+      } catch (error) {
+        this.alert = {
+          show: true,
+          type: 'error',
+          message: error.response.data.detail
+        }
+      }
+    }
   }
 };
 </script>
