a35d70e7543bf2ce9d68f7f029989e7b50a03d7b
DanLowo <danlowo@horbify.com>
2021-07-18 16:45:53 +0100

    firebase auth plugin

--- plugins/google-auth.js
+++ plugins/google-auth.js
@@ -4,6 +4,7 @@ import GAuth from 'vue-google-oauth2'
 const gauthOption = {
   clientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
   scope: 'profile email',
-  prompt: 'select_account'
+  prompt: 'select_account',
+  immediate: false
 }
 Vue.use(GAuth, gauthOption)
